de641f1f054c26ebad25d0e364e6e3a37f79628e
AddyKwach <adriankwach@gmail.com>
2025-06-09 00:55:10 +0300

    Footer urls updates

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -8,8 +8,8 @@ const Footer = () => {
       <div className={styles.container}>
         <p>© 2025 Adrian Kwach. All Rights Reserved.</p>
         <div className={styles.links}>
-          <Link href="https://github.com" target="_blank" rel="noopener noreferrer">GitHub</Link>
-          <Link href="https://linkedin.com" target="_blank" rel="noopener noreferrer">LinkedIn</Link>
+          <Link href="https://github.com/AddyKwach" target="_blank" rel="noopener noreferrer">GitHub</Link>
+          <Link href="https://www.linkedin.com/in/adrian-kwach-749a32226" target="_blank" rel="noopener noreferrer">LinkedIn</Link>
         </div>
       </div>
     </footer>
